7236607a215e3266fd738115ebee77e56e889e82
Loan Bich <tranbichloan25112002@gmail.com>
2025-01-15 06:41:27 +0700

    evaluate

--- src/models.py
+++ src/models.py
@@ -11,12 +11,19 @@ from src.feature_extractor import wav2vec2
 
 def fit_model(feature_vectors, labels):
     mlp = MLPClassifier(
-        hidden_layer_sizes=(40,),
-        alpha=1e-4,
-        solver="adam",
+        hidden_layer_sizes=(
+            86,
+            86,
+            40,
+            20,
+            10,
+        ),
+        solver="sgd",
         verbose=10,
+        tol=1e-5,
         random_state=1,
-        learning_rate_init=0.2,
+        learning_rate_init=1e-2,
+        max_iter=1000,
     )
 
     mlp.fit(feature_vectors, labels)
@@ -29,7 +36,7 @@ def predict_score(model, waveform, actual_label):
     proba = model.predict_proba(np.expand_dims(feature_vector, 0))
     score = proba[:, label2id(actual_label)]
 
-    return score
+    return renormalize(score, (0, 1), (1, 5))
 
 
 def save_model(model):
@@ -41,3 +48,9 @@ def load_model():
     with open(Path(__file__).parent / "model.pkl", "rb") as f:
         model = pickle.load(f)
     return model
+
+
+def renormalize(n, range1, range2):
+    delta1 = range1[1] - range1[0]
+    delta2 = range2[1] - range2[0]
+    return (delta2 * (n - range1[0]) / delta1) + range2[0]
